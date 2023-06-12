dab6fa67aae9676096d0525b013efca984969ad6
CptQuak <89586201+CptQuak@users.noreply.github.com>
2023-06-12 15:08:00 +0200

    Small refactor in processing code

--- scripts/preprocessing_yellow.py
+++ scripts/preprocessing_yellow.py
@@ -3,21 +3,23 @@ import geopandas as gpd
 import numpy as np
 from datetime import timedelta
 
+
+
 def data_missing_value_imputation(df):
     def value_imputation(df, column_name, fill_value):
         '''Function to fill missing rows with specific value'''
-        df[column_name] = df[column_name].fillna(fill_value) # which value to indicate missingnes
+        df[column_name] = df[column_name].fillna(fill_value)
         return df
     df = (df
         ## missing value imputation with values out of range of standard values that we can later recognize
         .pipe(value_imputation, column_name='RatecodeID', fill_value=0)
-        # gonna assume that it had to be stored in car if there was a problem with server connection
+        ## gonna assume that it had to be stored in car if there was a problem with server connection
         .pipe(value_imputation, column_name='store_and_fwd_flag', fill_value='Y')
-        # most of the time there there is a value of 2.5 so im gonna use it
+        ## most of the time there there is a value of 2.5 so im gonna use it
         .pipe(value_imputation, column_name='congestion_surcharge', fill_value=2.5)
-        # im gonna assume there is not airport fee, but mayby better idea is to do it by identifying starting district
+        ## im gonna assume there is not airport fee, but mayby better idea is to do it by identifying starting district
         .pipe(value_imputation, column_name='airport_fee', fill_value=0)
-        # passengers are problematic, for now assigning -1 value for problematics cases
+        ## passengers are problematic, for now assigning -1 value for problematics cases
         .pipe(value_imputation, column_name='passenger_count', fill_value=-1)
     )
     
@@ -30,7 +32,7 @@ def data_missing_value_imputation(df):
 
 def data_integrity_filtering(df, start_date, end_date):
     '''
-    performing filtering to remove observations with nonsense data, explaination in query
+    Performs filtering to remove observations with nonsense data, explaination in query
     '''
     df = df[
         ## only valid vendors and zones
@@ -40,20 +42,22 @@ def data_integrity_filtering(df, start_date, end_date):
         # also i found information that https://www.nyc.gov/site/tlc/passengers/passenger-frequently-asked-questions.page
         # there can be up to 4-5 passengers, and sometimes a child (so up to 6) can be transproted, also keeping imputed -1 indicator
         & ((df['passenger_count'].between(1, 6)) | (df['passenger_count'] == -1))
-        # # making sure that variables that have exactly specified values in documentation take those values
+        ## making sure that variables that have exactly specified values in documentation take those values
         & (df['RatecodeID'].between(0, 6) & df['store_and_fwd_flag'].isin(['Y', 'N']) & df['payment_type'].between(1, 6) 
         & df['airport_fee'].isin([0, 1.25]) & df['mta_tax'].isin([0, 0.5])) & (df['congestion_surcharge'].isin([0, 2.5])) 
-        # ## values that should always be non negative
+        ## values that should always be non negative
         & ((df['fare_amount'] >= 0) & (df['extra'] >= 0) & (df['tip_amount'] >= 0) & (df['tolls_amount'] >= 0) & (df['total_amount'] >= 0) )
         ## valid dates
         & ((df['tpep_dropoff_datetime'] >= f'{start_date}-01') & (df['tpep_dropoff_datetime'] <= f'{end_date}-01'))
         & ((df['tpep_pickup_datetime'] >= f'{start_date}-01') & (df['tpep_pickup_datetime'] <= f'{end_date}-01'))
-        # removing problematic cases where drop off happend before pickup or the trip took 0 seconds,
+        ## removing problematic cases where drop off happend before pickup or the trip took 0 seconds,
         & (df['tpep_dropoff_datetime'] > df['tpep_pickup_datetime'])
     ]
     return df
 
+
 def data_feature_engineering(df, loc_borough_map):
+    """Feature engineering associated with extractin time and localizatin related infromation"""
     df = (df
         ## adding simple information about traveling time in minutes
         .assign(trip_time_min=lambda x: (x['tpep_dropoff_datetime']-x['tpep_pickup_datetime'])/timedelta(minutes=1))
@@ -64,9 +68,9 @@ def data_feature_engineering(df, loc_borough_map):
     )
     return df
 
+
 def data_infered_filtering(df):
     '''This function is used to perform filtering on a dataframe by infromation infered from EDA'''
-
     df = df[
         # 99.8% of trips below 25 miles, there were higher values such as 1000miles with is most likely an error // len(df[df['trip_distance'] <= 25])/len(df)
         (df['trip_distance'] <= 25) 
@@ -83,8 +87,9 @@ def data_infered_filtering(df):
     ]
     return df
 
-def clean_yellow_taxi_df(df_path, year, month):
-    # preparation
+
+def define_start_end_date(year, month):
+    ## preparation for various months
     if month < 9:
         start_date, end_date = f'{year}-0{month}', f'{year}-0{month+1}'
     elif month == 9:
@@ -93,13 +98,24 @@ def clean_yellow_taxi_df(df_path, year, month):
         start_date, end_date = f'{year}-{month}', f'{year}-{month+1}'
     elif month == 12:
         start_date, end_date = f'{year}-{month}', f'{year+1}-01'
+    return start_date, end_date
 
+
+def define_zone_mapping(df_path):
+    ## dict mapping zone - borough
     df_zones = pd.read_csv(f'{df_path}/taxi_zones/taxi+_zone_lookup.csv').iloc[:-2, :]
     zones_mapping = df_zones[['LocationID', 'Borough']].to_dict()
     keys = list(zones_mapping.keys())
     loc_borough_map = {zones_mapping[keys[0]][i]: zones_mapping[keys[1]][i]  for i in range(len(zones_mapping['LocationID']))}
+    return loc_borough_map
 
-    # processing
+
+def clean_yellow_taxi_df(df_path, year, month):
+    ## mapping constants
+    start_date, end_date = define_start_end_date(year, month)
+    loc_borough_map = define_zone_mapping(df_path)
+
+    ## processing data
     df = pd.read_parquet(f'{df_path}/yellow_tripdata_{start_date}.parquet')
     df = data_missing_value_imputation(df)
     df = data_integrity_filtering(df, start_date, end_date)
